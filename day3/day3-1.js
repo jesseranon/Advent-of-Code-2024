d035aa8c2bad872cdfb74fd506a5533625dfae7b
jesseranon <jessean.infiltrator@gmail.com>
2024-12-15 01:08:23 -0800

    solved day3 part2

--- day3/day3-1.js
+++ day3/day3-1.js
@@ -1,4 +1,5 @@
 import {readFile} from '../helperFunctions.js'
+import { part2InstructionRegex } from './day3-2.js'
 
 const inputString = readFile('./day3/day3input.txt')
 
@@ -16,16 +17,26 @@ function mul(string) {
 }
 
 function runInstructions(instructionsArray) {
-    let sumResults = 0 
+    let sumResults = 0
+    let mulEnabled = true
     for (let i = 0; i < instructionsArray.length; i++) {
         const instruction = instructionsArray[i]
-        const args = instruction.match(/[0-9]{1,3},[0-9]{1,3}/)[0]
-        sumResults += mul(args)
+        if (instruction === 'do()') mulEnabled = true
+        if (instruction === 'don\'t()') mulEnabled = false
+        if (mulEnabled && instruction.indexOf('mul') >= 0) {
+            const args = instruction.match(/[0-9]{1,3},[0-9]{1,3}/)[0]
+            sumResults += mul(args)
+        }
     }
     return sumResults
 }
 
-const instructionsArray = matchUncorruptedInstructions(inputString, instructionRegex)
+// const instructionsArray = matchUncorruptedInstructions(inputString, instructionRegex)
+
+
+// part 2
+
+const instructionsArray = matchUncorruptedInstructions(inputString, part2InstructionRegex)
 
 const result = runInstructions(instructionsArray)
 
---
+++ day3/day3-2.js
@@ -0,0 +1,1 @@
+export const part2InstructionRegex = /(do|don't|mul)\(([0-9]{1,3},[0-9]{1,3}){0,1}\)/g
